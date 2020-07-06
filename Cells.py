1c136fff475ac27affece8156a9fa7361f2bb60e
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-07-06 12:44:55 +0200

    Create Cells.py

---
+++ Cells.py
@@ -0,0 +1,118 @@
+from Grid import Grid
+from PIL import Image, ImageDraw
+import getpass
+host = getpass.getuser()
+
+
+def Cells(HGrid, VGrid):
+	def cells(grid, Cells):
+		for x in range(len(grid)-1):
+			cell = [y for y in range(grid[x]+1, grid[x+1])]
+			Cells.append(cell)
+	
+	XCells = []
+	YCells = []
+	cells(HGrid, YCells)
+	cells(VGrid, XCells)
+	return XCells, YCells
+	
+def drawCells(X,Y, color, source):
+
+	Im = Image.open(source)
+	draw = ImageDraw.Draw(Im)
+	for y in Y:
+		for x in X:
+			draw.point([x, y], color)
+	Im.save(r"C:\Users\{}\Desktop\Grid.png".format(host))
+
+
+def nextGenLive(CurrentCells):
+	global nextGen
+	nextGen = []
+	nextGenDel = []
+	neighbours = []
+	
+	for cell in CurrentCells:
+		numLiveCells = 0
+		
+		if [cell[0]+1, cell[1]] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]+1, cell[1]] not in neighbours:
+			neighbours += [[cell[0]+1, cell[1]]]
+
+		if [cell[0]-1, cell[1]] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]-1, cell[1]] not in neighbours:
+			neighbours += [[cell[0]-1, cell[1]]]
+
+		
+		if [cell[0], cell[1]+1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0], cell[1]+1] not in neighbours:
+			neighbours += [[cell[0], cell[1]+1]]
+
+		if [cell[0], cell[1]-1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0], cell[1]-1] not in neighbours:
+			neighbours += [[cell[0], cell[1]-1]]
+
+		if [cell[0]+1, cell[1]+1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]+1, cell[1]+1] not in neighbours:
+			neighbours += [[cell[0]+1, cell[1]+1]]
+		
+		if [cell[0]+1, cell[1]-1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]+1, cell[1]-1] not in neighbours:
+			neighbours += [[cell[0]+1, cell[1]-1]]
+		
+		if [cell[0]-1, cell[1]+1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]-1, cell[1]+1] not in neighbours:
+			neighbours += [[cell[0]-1, cell[1]+1]]
+
+		
+		if [cell[0]-1, cell[1]-1] in CurrentCells:
+			numLiveCells +=1
+		elif [cell[0]-1, cell[1]-1] not in neighbours:
+			neighbours += [[cell[0]-1, cell[1]-1]]	
+		
+		
+		if (numLiveCells == 2 or numLiveCells == 3) and cell not in nextGen:
+			nextGen += [cell]
+
+		if numLiveCells > 3 or numLiveCells < 2:
+			nextGenDel += [cell]
+	
+
+	for Cell in neighbours:
+		NumLiveCells = 0
+		if [Cell[0]+1, Cell[1]] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0]-1, Cell[1]] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0], Cell[1]+1] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0], Cell[1]-1] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0]+1, Cell[1]+1] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0]+1, Cell[1]-1] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0]-1, Cell[1]+1] in CurrentCells:
+			NumLiveCells +=1
+
+		if [Cell[0]-1, Cell[1]-1] in CurrentCells:
+			NumLiveCells +=1
+		
+		if NumLiveCells == 3 and Cell not in nextGen:
+			nextGen += [Cell]
+
+
+	return nextGen, nextGenDel
